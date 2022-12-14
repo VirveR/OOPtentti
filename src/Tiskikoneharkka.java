f6222764ddd34d17c47a1da46efb2adde5a7b02e
VirveR <112824021+VirveR@users.noreply.github.com>
2022-12-14 19:00:24 +0200

    Tiskikone-luokka luotu

--- src/Tiskikoneharkka.java
+++ src/Tiskikoneharkka.java
@@ -1,4 +1,3 @@
-
 public class Tiskikoneharkka {
 
 	public static void main(String[] args) {
@@ -6,4 +5,36 @@ public class Tiskikoneharkka {
 
 	}
 
-}
+}//main
+
+class Tiskikone {
+	//attribuutit
+	private int ohjelma = 0;
+	private boolean paalla = false;
+	
+	//rakentajat
+	public Tiskikone() {
+		ohjelma = 0;
+		paalla = false;
+	}
+	
+	public Tiskikone(int ohjelma, boolean paalla) {
+		this.ohjelma = ohjelma;
+		this.paalla = paalla;
+	}
+	
+	//setterit ja getterit
+	public void setOhjelma(int ohjelma) {
+		this.ohjelma = ohjelma;
+	}
+	public int getOhjelma() {
+		return this.ohjelma;
+	}
+	public void setPaalla(boolean paalla) {
+		this.paalla = paalla;
+	}
+	public boolean getPaalla() {
+		return this.paalla;
+	}
+
+}//tiskikone
